1846b5889321ccc8c7fd1fcb618a0b3e827abebe
Geraldine <geraldine@g-angle.co.jp>
2017-02-14 17:33:07 +0800

    Add css for title page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'axa-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [`
+      #container{
+        width: 1707px;
+        height: 1067px;
+        background-color: #F8F8F8;
+      }
+    `]
 })
 export class AppComponent {
   title = 'axa works!';
