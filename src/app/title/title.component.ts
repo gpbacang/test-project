4cb1624ab47dca35ead7395fb7a25f9f28b540c7
Geraldine <geraldine@g-angle.co.jp>
2017-02-14 17:13:23 +0800

    Add title component

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'axa-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./style/title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  title="this is the title";
+
+}
